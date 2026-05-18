5b3500f9dd1b667a914cc82c60cc10ddc94f5e46
KrypZero <105665333+KrypZero@users.noreply.github.com>
2026-05-18 23:13:23 +0200

    Update hinnkaDE.sh

--- hinnkaDE.sh
+++ hinnkaDE.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-feh --bg-fill /usr/local/bin/hinnkaDE/Wallpaper.png &
+feh --bg-fill sudo cp Wallpaper.png /usr/local/bin/hinnkaDE/Wallpaper.png &
+polybar &
 xfce4-panel &
 openbox-session
